064c8fc624e6b441f0dc357780a377f51a51d5a0
Andrew Hofmeister <hofmeister.andy@gmail.com>
2019-12-08 19:42:56 -0600

    added get results function

--- fetch/models.py
+++ fetch/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
-class Races(models.Model):
+class Race(models.Model):
 	race_id = models.IntegerField(primary_key=True) #Clubspeed id number
 	date = models.DateTimeField() #Scheduled Race Time
 	race_name = models.CharField(max_length=30) # Heat 1, 2, Main
 	
 	def __str__(self):
 		return self.race_name + ' ' + self.date
+
+class Result(models.Model):
+	race = models.ForeignKey(Race, on_delete=models.CASCADE)
+	position = models.IntegerField()
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	racer_id = models.IntegerField()
+	kart_num = models.IntegerField()
+	gap = models.FloatField()
+	fastest_lap_time = models.FloatField()
--- fetch/views.py
+++ fetch/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from datetime import date, timedelta
 from dotenv import load_dotenv
 
+from .models import Race, Result
+
 load_dotenv()
 API_KEY = os.getenv('CLUBSPEED_API_KEY')
 
@@ -22,10 +24,31 @@ def findRaces(race_date):
 	parameters = {
 		"key" : API_KEY
 	}
-	league_races = {}
 	for n in race_ids:
 		response=requests.get("https://ssnewcaney.clubspeedtiming.com/api/index.php/races/" + n + ".json", params=parameters)
 		race = response.json()['race']
 		if race['race_name'] in ('Qualifying', 'Heat 1', 'Heat 2', 'Main'):
-			races = Races(race_id = n, date = race['starts_at'], race_name = race['race_name'])
+			races = Race(race_id = n,
+				date = race['starts_at'],
+				race_name = race['race_name']
+				)
 			races.save()
+
+def getRaceResults(race_id):
+	parameters = {
+	"key" : API_KEY,
+	}
+	response = requests.get("https://ssnewcaney.clubspeedtiming.com/api/index.php/races/" + race_id + ".json", params=parameters)
+	scoreboard = response.json()['scoreboard']
+	for n in scoreboard:
+		result = Result(
+			race = race_id, 
+			position = scoreboard['position'],
+			first_name = scoreboard['first_name'],
+			last_name = scoreboard['last_name'],
+			racer_id = ['racer_id'],
+			kart_num = scoreboard['kart_num'],
+			gap = scoreboard['gap'],
+			fastest_lap_time = scoreboard['fastest_lap_time']
+			)
+		result.save()
