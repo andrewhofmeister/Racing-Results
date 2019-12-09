d6a51f4f3eedc2e773e6784798c665d4e2218832
Andrew Hofmeister <hofmeister.andy@gmail.com>
2019-12-09 17:55:17 -0600

    refactoring again....

--- standings/admin.py
+++ standings/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-
-from .models import Round
-from fetch.models import Races
-
-admin.site.register(Round)
-admin.site.register(Races)
--- standings/models.py
+++ standings/models.py
@@ -30,3 +30,22 @@ class Results(models.Model):
 
 class Round(models.Model):
 	date = models.DateTimeField()
+
+
+class Race(models.Model):
+	race_id = models.IntegerField(primary_key=True) #Clubspeed id number
+	date = models.DateTimeField() #Scheduled Race Time
+	race_name = models.CharField(max_length=30) # Heat 1, 2, Main
+	
+	def __str__(self):
+		return self.race_id + '-' + self.date
+
+class Result(models.Model):
+	race_id = models.IntegerField(primary_key=True)
+	position = models.IntegerField()
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)
+	racer_id = models.IntegerField()
+	kart_num = models.IntegerField()
+	gap = models.FloatField()
+	fastest_lap_time = models.FloatField()
